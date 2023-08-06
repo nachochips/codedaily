e400d406d5b70635f5fc30163b6082b55a36b4b8
Nachochips <68362149+nachochips@users.noreply.github.com>
2023-08-06 09:35:58 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -1,8 +1,7 @@
 def solution(my_string, overwrite_string, s):
     original = list(my_string)
-    overwrite = list(overwrite_string)
     endnumber = len(overwrite_string)+s
-    original[s:endnumber] = overwrite
+    original[s:endnumber] = overwrite_string
     answer = "".join(original)
 
     return answer
