e274136cb09b6e243caa051c8d6e85be4508c9fb
Nachochips <68362149+nachochips@users.noreply.github.com>
2023-08-08 14:46:06 +0900

    Update 문자열 섞기.py

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.py
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.py
@@ -4,4 +4,9 @@ def solution(str1, str2):
     for i in range(l):
         answer += (str1[i] + str2[i])     
     return answer
-    
+
+
+#improved version by Chat GPT 
+def solution(str1, str2):
+    answer = [str1[i] + str2[i] for i in range(len(str1))]
+    return ''.join(answer)
